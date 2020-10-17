3cd5dede0f8cf1a095a35a0e217bb1bb182c4dab
Paolo Posso <pvictorsys@gmail.com>
2020-10-17 15:51:40 -0300

    add website handler and trait

--- src/http/response.rs
+++ src/http/response.rs
@@ -1,5 +1,3 @@
-use std::fmt::{Display, Formatter, Result as FmtResult};
-use std::net::TcpStream;
 use std::io::{Write, Result as IoResult};
 use super::StatusCode;
 
@@ -13,7 +11,7 @@ impl Response {
         Response { status_code, body }
     }
 
-    pub fn send(&self, stream: &mut TcpStream) -> IoResult<()> {
+    pub fn send(&self, stream: &mut impl Write) -> IoResult<()> {
         let body = match &self.body {
             Some(b) => b,
             None => ""
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@
 
 mod server;
 mod http;
+mod website_handler;
 
 use server::Server;
+use website_handler::WebsiteHandler;
 
 fn main() {
     let server = Server::new(String::from("127.0.0.1:8080"));
-    server.run();
+    server.run(WebsiteHandler);
 }
